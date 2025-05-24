54b311ea622ceab094f26587eb6d9e0a7487164e
Viorel <krakudze21@gmail.com>
2025-05-24 19:19:18 +0300

    Полный рабочий код

---
+++ models.py
@@ -0,0 +1,17 @@
+﻿class Product:
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+class Category:
+    total_categories = 0
+    total_unique_products = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+        Category.total_categories += 1
+        Category.total_unique_products += len(set(products))
---
+++ test_models.py
@@ -0,0 +1,10 @@
+﻿import pytest
+from models import Product, Category
+
+@pytest.fixture
+def sample_product():
+    return Product("Телефон", "Смартфон", 50000.0, 10)
+
+def test_product_init(sample_product):
+    assert sample_product.name == "Телефон"
+    assert sample_product.price == 50000.0
